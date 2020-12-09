804f53e4c02e2fcaf43dfcefd7cbd30a6ae88ba4
SultanRaka <raka.bagas@gmail.com>
2020-12-10 02:52:46 +0700

    Update Detector.py
    Added buzzer, changes in messages and broker address

--- Raspberry Pi Source Code/Detector.py
+++ Raspberry Pi Source Code/Detector.py
@@ -8,6 +8,7 @@ import threading
 
 led = 16
 button = 36
+buzzer = 37
 reader = SimpleMFRC522()
 status = 'Alive'
 
@@ -17,13 +18,17 @@ GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 
+
 def on(led):
     global status
     GPIO.setup(led, GPIO.OUT)
+    GPIO.setup(buzzer, GPIO.OUT)
     GPIO.output(led, GPIO.HIGH)
+    GPIO.output(buzzer, GPIO.HIGH)
     status = 'Dead'
 
 def off(led):
+    GPIO.output(buzzer, GPIO.LOW)
     GPIO.cleanup(led)
     
 def get_rfid():
@@ -34,9 +39,13 @@ def get_rfid():
         print('Revived!')
         status = 'Alive'
         client.publish("BangSultanSmartVestSystemForTA","Revived!")
+        time.sleep(3)
+        client.publish("BangSultanSmartVestSystemForTA","Alive")
     else:
         print('Player still alive!')
         client.publish("BangSultanSmartVestSystemForTA","Player still alive!")
+        time.sleep(3)
+        client.publish("BangSultanSmartVestSystemForTA","Alive")
     time.sleep(.1)
     get_rfid()
     
@@ -44,16 +53,16 @@ def get_button():
     if not GPIO.input(button):
         print('Player is dead!')
         on(led)
-        client.publish("BangSultanSmartVestSystemForTA","Mati!")
+        client.publish("BangSultanSmartVestSystemForTA","Dead")
     time.sleep(.1)
     get_button()
     
 try:
-    broker_address="192.168.2.123"
+    broker_address="192.168.2.69"
     client = mqtt.Client("P1")
     print('connecting')
     client.connect(broker_address)
-    client.publish("BangSultanSmartVestSystemForTA","Dari Raspi gan")
+    client.publish("BangSultanSmartVestSystemForTA","Alive")
     print('connected!')
     t1 = threading.Thread(target=get_button)
     t2 = threading.Thread(target=get_rfid)
