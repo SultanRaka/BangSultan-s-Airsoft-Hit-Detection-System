665118879be168e6af8ef1c2559eef09785dcaae
SultanRaka <raka.bagas@gmail.com>
2021-02-27 16:19:35 +0700

    Create Strike Plate Detector.ino
    This is the base code

---
+++ Arduino Uno/Strike Plate Detector.ino
@@ -0,0 +1,17 @@
+const int knockSensor = A0; // the piezo is connected to analog pin 0
+const int threshold = 100;  // threshold value to decide when the detected sound is a knock or not
+
+
+// these variables will change:
+int sensorReading = 0;      // variable to store the value read from the sensor pin
+
+void setup() {
+  Serial.begin(9600);       // use the serial port
+}
+
+void loop() {
+  // read the sensor and store it in the variable sensorReading:
+  sensorReading = analogRead(knockSensor);
+  Serial.println(sensorReading);
+  delay(100);  // delay to avoid overloading the serial port buffer
+}
